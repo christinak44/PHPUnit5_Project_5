5c08ebab9fab66003e4874f96222557cf0d5e5c0
Christina King <kingcm41@hotmail.com>
2020-01-09 11:13:19 -0500

    Comment class
    cleaned up existing code in routes file; completed comment class

--- My_Slim_Journal/src/routes.php
+++ My_Slim_Journal/src/routes.php
@@ -4,22 +4,20 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Classes\Entry;
+use App\Classes\Comment;
 return function (App $app) {
     $container = $app->getContainer();
+    // Display list of entries on Blog home page
     $app->get(/*['GET','POST'], */'/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
-        // Render index view
+
         $entries = Entry::all();
-        //$entry = new Entry($this->db);
-        //$entries = $entry->getEntries();
-        //var_dump($entries);
-        // $args = array_merge($args, $entries);
-        //$args['post'] = $this->db;
+
         $args['entries'] = $entries;
         return $this->view->render($response, 'Blog_Home.twig', $args);
     })->SetName('Blog_Home');
-
+   //create new entry for blog
     $app->map(['GET', 'POST'],'/new', function (Request $request, Response $response, array $args) use ($container) {
       if($request->getMethod() == "POST") {
           $args = array_merge($args, $request->getParsedBody());
@@ -33,24 +31,21 @@ return function (App $app) {
               return $response->withStatus(302)->withHeader('Location', $url);
             }
       }
-                //echo "Entry created successfully!";
+
         return $this->view->render($response, 'new.twig', $args);
    });
-
+    //load single entry details
     $app->get('/entry/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
 
-        //$entry = new Entry($this->db);
 
-        //$entries = $entry->getEntry($args['id']);
-
-        //$entry = Entry::where('id','=', $id);
         $entry = Entry::find($args['id']);
-        //$args = array_merge($args, $request->getParsedBody());
-        //$args['post'] = $this->db;
+
         $args['details'] = $entry;
-        //$args['entry'] = $entries;
+
         return $this->view->render($response, 'detail.twig', $args);
     })->SetName('Entry');;
+
+    //retrieve entry in edit view
     $app->map(['GET', 'POST'],'/entry/edit/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
         $entry = Entry::find($args['id']);
         $args['details'] = $entry;
@@ -58,18 +53,18 @@ return function (App $app) {
       if($request->getMethod() == "POST") {
          $args = array_merge($args, $request->getParsedBody());
              if (!empty($args['title']) && !empty($args['body'])) {
-            //   $entry = new Entry;
+
                $entry->title = $args['title'];
                $entry->body = $args['body'];
                $entry->save();
-             //$args['details'] = $entry;
+
              $args['message'] = "Entry updated successfully!";
              $url = "/entry/" . $args['id'];
-      return $response->withStatus(302)->withHeader('Location', $url);
+
+      return $this->view->render($response, 'detail.twig', $args);
             }
       }
 
-                //echo "Entry created successfully!";
 
 
         return $this->view->render($response, 'edit.twig', $args);
